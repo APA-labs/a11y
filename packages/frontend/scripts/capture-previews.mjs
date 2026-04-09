2122d1be3176f4e5a29076a554ae161b2301156e
Zero-1016 <iolpd42@gmail.com>
2026-04-09 17:29:59 +0900

    chore(frontend): alert, alert-dialog, table, toast 프리뷰 이미지 재캡처
    1200x800이던 4개 프리뷰를 나머지 22개와 동일한 498x398 @2x(996x796) 포맷으로 재캡처.
    Playwright + MUI v5 UMD CDN 사용, 캡처 스크립트 포함.

--- packages/frontend/scripts/capture-previews.mjs
+++ packages/frontend/scripts/capture-previews.mjs
@@ -42,7 +42,7 @@ function baseHTML(bodyScript) {
   </style>
   <script src="https://unpkg.com/react@18/umd/react.production.min.js" crossorigin></script>
   <script src="https://unpkg.com/react-dom@18/umd/react-dom.production.min.js" crossorigin></script>
-  <script src="https://unpkg.com/@mui/material@latest/umd/material-ui.production.min.js" crossorigin></script>
+  <script src="https://unpkg.com/@mui/material@5/umd/material-ui.production.min.js" crossorigin></script>
 </head>
 <body>
   <div id="root"></div>
